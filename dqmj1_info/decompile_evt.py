43373540489ce0da261abbd6bdd188f613431423
Christopher Wells <cwellsny@nycap.rr.com>
2024-01-31 18:35:59 -0800

    Rename script and add flag to skip unknown commands

--- dqmj1_info/decompile_evt.py
+++ dqmj1_info/decompile_evt.py
@@ -9,7 +9,8 @@ import evt
 def main(argv: List[str]):
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--evt_filepath")
+    parser.add_argument("--evt_filepath", required=True)
+    parser.add_argument("--ignore_unknown_commands", default=False, action="store_true")
 
     args = parser.parse_args(argv)
 
@@ -17,9 +18,8 @@ def main(argv: List[str]):
         event = evt.Event.from_evt(input_stream)
 
     for command in event.commands:
-        if not isinstance(command, evt.UnknownCommand):
+        if args.ignore_unknown_commands or not isinstance(command, evt.UnknownCommand):
             print(command)
-        # print(command)
 
 
 if __name__ == "__main__":

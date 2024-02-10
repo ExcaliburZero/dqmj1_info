1760ef4e02f2736534000afeb54830615ffda662
Christopher Wells <cwellsny@nycap.rr.com>
2024-02-10 02:59:53 -0800

    Add regression test for script compilation
    Re-using code from another project I wrote.
    
    https://github.com/ExcaliburZero/cbpickaxe/tree/master/regression_tests

---
+++ regression_tests/test_regression_compile_evt.py
@@ -0,0 +1,25 @@
+import inspect
+import sys
+
+from .util import Util, rel_data, update_baselines
+
+
+class TestRegressionCompileEvtBasic(Util.TestRegression):
+    name = "compile_evt_basic"
+    command = [
+        "dqmj1_compile_evt",
+        "--script_filepaths",
+        rel_data("basic.dqmj1_script"),
+        "--output_directory",
+        ".",
+    ]
+    expected_files = ["translated_strings.csv"]
+
+
+if __name__ == "__main__":
+    tests = []
+    for name, obj in inspect.getmembers(sys.modules[__name__]):
+        if inspect.isclass(obj) and obj != Util:
+            tests.append(obj)
+
+    update_baselines(tests)

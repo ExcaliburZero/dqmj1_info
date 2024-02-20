95a988b4488bc7d37a53595aec9e48f5d6d08fa5
Christopher Wells <cwellsny@nycap.rr.com>
2024-02-19 16:40:57 -0800

    Start trying to extract some info from EU release

--- dqmj1_info/language_configs/__init__.py
+++ dqmj1_info/language_configs/__init__.py
@@ -1,2 +1,3 @@
 from .language_configs import *
 from . import en
+from . import eu
---
+++ dqmj1_info/language_configs/eu.py
@@ -0,0 +1,26 @@
+from .language_configs import *
+
+LANGUAGE_CONFIGS["eu"] = LanguageConfig(
+    string_tables={
+        pathlib.Path("arm9.bin"): (
+            0x02000000,
+            [
+                StringTable("strings_a", 0x020763C0, 0x02076B30),
+                StringTable("strings_b", 0x02075FF0, 0x02076138),
+            ],
+        ),
+        pathlib.Path("overlay")
+        / "overlay_0000.bin": (
+            0x021A0A00,
+            [],
+        ),
+    },
+    string_address_tables={
+        pathlib.Path("arm9.bin"): (
+            0x02000000,
+            [
+                StringAddressTable("unknown_a", 0x0207616C, 0x020763C0),
+            ],
+        ),
+    },
+)

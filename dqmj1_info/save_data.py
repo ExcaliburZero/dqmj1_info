ed4087d91db0e6f1e342d97c2b7877074fbad478
Christopher Wells <cwellsny@nycap.rr.com>
2024-09-21 23:45:55 -0700

    Create script to summarize save files

--- dqmj1_info/save_data.py
+++ dqmj1_info/save_data.py
@@ -1,7 +1,10 @@
 from dataclasses import dataclass
-from typing import IO, Literal
+from typing import IO, List, Literal, Tuple
 
+import io
 import sys
+
+from .character_encoding import CharacterEncoding
 
 HEADER_SIZE = 0x70
 DATA_SIZE = 10613
@@ -51,6 +54,59 @@ class SaveDataRaw:
 
     def write_sav(self, output_stream: IO[bytes]) -> None:
         output_stream.write(self.raw)
+
+
+@dataclass
+class MonsterPreview:
+    name: str
+    level: int
+
+    @staticmethod
+    def multiple_from_raw(
+        input_stream: IO[bytes], character_encoding: CharacterEncoding
+    ) -> List["MonsterPreview"]:
+        names = []
+        for _ in range(0, 3):
+            input_stream.read(3)
+            names.append(character_encoding.bytes_to_string(input_stream.read(9)))
+
+        input_stream.read(9)
+
+        levels = [int.from_bytes(input_stream.read(1)) for _ in range(0, 3)]
+
+        return [
+            MonsterPreview(name=name, level=level) for name, level in zip(names, levels)
+        ]
+
+
+@dataclass
+class SaveData:
+    player_name: str
+    party_previews: List[MonsterPreview]
+
+    @staticmethod
+    def from_sav(
+        input_stream: IO[bytes], character_encoding: CharacterEncoding
+    ) -> Tuple["SaveData", SaveDataRaw]:
+        raw = SaveDataRaw.from_sav(input_stream)
+
+        return SaveData.from_raw(raw, character_encoding), raw
+
+    @staticmethod
+    def from_raw(raw: SaveDataRaw, character_encoding: CharacterEncoding) -> "SaveData":
+        input_stream = io.BytesIO(raw.raw)
+
+        input_stream.read(48)
+        num_party_monsters = int.from_bytes(input_stream.read(1))
+
+        input_stream.read(1)
+        player_name = character_encoding.bytes_to_string(input_stream.read(9))
+
+        party_previews = MonsterPreview.multiple_from_raw(
+            input_stream, character_encoding
+        )[0:num_party_monsters]
+
+        return SaveData(player_name=player_name, party_previews=party_previews)
 
 
 if __name__ == "__main__":
---
+++ dqmj1_info/summaraize_save_file.py
@@ -0,0 +1,58 @@
+from typing import List
+
+import argparse
+import csv
+import logging
+import pathlib
+import sys
+
+from .character_encoding import CHARACTER_ENCODINGS
+from .save_data import SaveData
+
+
+def main(argv: List[str]) -> None:
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--save_data_filepaths", required=True, nargs="+", type=pathlib.Path
+    )
+    parser.add_argument("--output_filepath", required=True, type=pathlib.Path)
+    parser.add_argument("--character_encoding", required=True)
+
+    args = parser.parse_args(argv)
+
+    save_data_filepaths: List[pathlib.Path] = args.save_data_filepaths
+    output_filepath: pathlib.Path = args.output_filepath
+
+    character_encoding = CHARACTER_ENCODINGS[args.character_encoding]
+
+    save_data_list = []
+    for save_data_filepath in save_data_filepaths:
+        logging.debug(f"Looking at save data file: {save_data_filepath}")
+        with open(save_data_filepath, "rb") as input_stream:
+            save_data, raw = SaveData.from_sav(input_stream, character_encoding)
+
+        save_data_list.append((save_data_filepath, save_data, raw))
+
+    columns = ["Filepath", "Player name", "Party"]
+    with open(output_filepath, "w", encoding="utf-8") as output_stream:
+        writer = csv.DictWriter(output_stream, fieldnames=columns)
+        writer.writeheader()
+
+        for filepath, save_data, raw in save_data_list:
+            writer.writerow(
+                {
+                    "Filepath": filepath,
+                    "Player name": save_data.player_name,
+                    "Party": ", ".join(
+                        [
+                            f"{preview.name} lv.{preview.level}"
+                            for preview in save_data.party_previews
+                        ]
+                    ),
+                }
+            )
+
+
+def main_without_args() -> None:
+    main(sys.argv[1:])

74caf768cadb9895de47ba1fb4b69787f454b924
Christopher Wells <cwellsny@nycap.rr.com>
2024-09-28 22:24:06 -0700

    Create script to parse info from BtlEnmyPrm file

---
+++ dqmj1_info/btl_enmy_prm.py
@@ -0,0 +1,90 @@
+from dataclasses import dataclass
+from typing import IO, List, Literal
+
+import argparse
+import logging
+import sys
+
+import pandas as pd
+
+ENDIANESS: Literal["little"] = "little"
+
+
+@dataclass
+class BtlEnmyPrmEntry:
+    species_id: int
+    level: int
+    max_hp: int
+    max_mp: int
+    attack: int
+    defense: int
+    agility: int
+    wisdom: int
+
+    @staticmethod
+    def from_bin(input_stream: IO[bytes]) -> "BtlEnmyPrmEntry":
+        species_id = int.from_bytes(input_stream.read(2), ENDIANESS)
+
+        input_stream.read(46)
+        level = int.from_bytes(input_stream.read(2), ENDIANESS)
+
+        input_stream.read(2)
+        max_hp = int.from_bytes(input_stream.read(2), ENDIANESS)
+        max_mp = int.from_bytes(input_stream.read(2), ENDIANESS)
+        attack = int.from_bytes(input_stream.read(2), ENDIANESS)
+        defense = int.from_bytes(input_stream.read(2), ENDIANESS)
+        agility = int.from_bytes(input_stream.read(2), ENDIANESS)
+        wisdom = int.from_bytes(input_stream.read(2), ENDIANESS)
+
+        input_stream.read(20)
+        skill_set_ids = [int.from_bytes(input_stream.read(1)) for _ in range(0, 3)]
+
+        input_stream.read(1)
+
+        return BtlEnmyPrmEntry(
+            species_id=species_id,
+            level=level,
+            max_hp=max_hp,
+            max_mp=max_mp,
+            attack=attack,
+            defense=defense,
+            agility=agility,
+            wisdom=wisdom,
+        )
+
+
+@dataclass
+class BtlEnmyPrm:
+    entries: List[BtlEnmyPrmEntry]
+
+    @staticmethod
+    def from_bin(input_stream: IO[bytes]) -> "BtlEnmyPrm":
+        magic = int.from_bytes(input_stream.read(4), ENDIANESS)
+        length = int.from_bytes(input_stream.read(4), ENDIANESS)
+
+        entries = []
+
+        for i in range(0, length):
+            entries.append(BtlEnmyPrmEntry.from_bin(input_stream))
+
+        return entries
+
+
+def main(argv: List[str]) -> None:
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--table_filepath", required=True)
+    parser.add_argument("--output_csv", required=True)
+
+    args = parser.parse_args(argv)
+
+    with open(args.table_filepath, "rb") as input_stream:
+        table = BtlEnmyPrm.from_bin(input_stream)
+
+    logging.debug(f"Read {len(table)} entries from: {args.table_filepath}")
+
+    pd.DataFrame(table).to_csv(args.output_csv, index=False)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
